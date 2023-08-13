20f0328a65a9cd5396571544fcefdf6f43b8eafa
Omar Omran <omaromran2003@gmail.com>
2023-08-14 02:34:04 +0400

    catch jwt errors

--- api/utils.py
+++ api/utils.py
@@ -1,8 +1,10 @@
 import datetime
 
-from api.database.init_db import SessionLocal
+from fastapi import HTTPException, status
+
+from api.db.init_db import SessionLocal
 from datetime import timedelta
-from jose import jwt
+from jose import jwt, JWTError
 
 SECRET_KEY = "1e0788a28e2e503315a3a894d353abaa36ace075faae8650f714d7c880f01da5"
 ALGORITHM = "HS256"
@@ -17,13 +19,14 @@ def get_db():
         db.close()
 
 
-def create_token(payload: dict, duration: int | None = ACCESS_TOKEN_EXPIRE_MINUTES):
+def create_token(payload: dict, duration: int = ACCESS_TOKEN_EXPIRE_MINUTES):
     payload["exp"] = datetime.datetime.now() + timedelta(minutes=duration)
     return jwt.encode(payload, SECRET_KEY, ALGORITHM)
 
 
 def decode_token(token):
-    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
-
-print(decode_token(create_token({"username":"omar"})))
