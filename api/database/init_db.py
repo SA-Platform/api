9a21fd012482efcd470b2ae604c8f2cd5bb7f02d
Omar Omran <omaromran2003@gmail.com>
2023-08-14 02:36:40 +0400

    just forget to include this one delete database folder

--- api/database/init_db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from api.database.models import Base
-
-engine = create_engine("sqlite:///platform.db")
-Base.metadata.create_all(engine)
-SessionLocal = sessionmaker(bind=engine)
