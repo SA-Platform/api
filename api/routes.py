35e4e8c5e3b4f91edb1536b97b2bdb69d8c4ab56
Omar Omran <omaromran2003@gmail.com>
2023-08-14 02:34:32 +0400

    add signin and getusers api

--- api/routes.py
+++ api/routes.py
@@ -1,28 +1,36 @@
-
-from typing import Union
-
-from fastapi import FastAPI, status, Depends
-
-from api.validators import UserValidator
-from api.database.models import User
-from api import SessionLocal
+from fastapi import FastAPI, status, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from api.validators import UserSignup
+from api.db.models import User
+from api.utils import get_db, create_token
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
-@app.post("/signup", tags=["Users"])
-async def signup(request: UserValidator, db: Session = Depends(get_db)):
+@app.post("/signup", tags=["Users"], status_code=status.HTTP_201_CREATED)
+async def signup(request: UserSignup, db: Session = Depends(get_db)):
     new_user = User(**request.model_dump())
     db.add(new_user)
     db.commit()
-    return new_user
+    return "user created successfully"
 
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="gettoken")
 
+
+@app.post("/signin", tags=["Users"], status_code=status.HTTP_200_OK)
+async def signin(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter((User.email == form_data.username) | (User.username == form_data.username)).first()
+    if user:
+        if user.check_password(form_data.password):
+            return {"access_token": create_token({"username": user.username}), "token_type": "bearer"}
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="incorrect password")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="user not found")  # for debugging only, should be changed to raise unauthorized always
+
+
+@app.get("/getusers")
+async def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
