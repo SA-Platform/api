8f94998fc54fe47911703b79d2ed33780962245e
Omar Omran <omaromran2003@gmail.com>
2023-08-15 01:45:58 +0400

    add announcement pydantic validator

--- api/validators.py
+++ api/validators.py
@@ -1,5 +1,14 @@
-from pydantic import BaseModel, Field, EmailStr, DirectoryPath
+from pydantic import BaseModel, Field, EmailStr, field_validator
 from datetime import datetime
+from enum import Enum
+
+
+class AnnouncementsCategory(str, Enum):
+    INTERNSHIP = "internship"
+    EVENT = "event"
+    WORKSHOP = "workshop"
+    COMPETITION = "competition"
+    OTHER = "other"
 
 
 class UserSignup(BaseModel):
@@ -33,5 +42,31 @@ class UserSignup(BaseModel):
                 "faculty_department": "electrical",
                 "graduation_year": "2025",
                 "image_file": r"C:\Users\omaro\Desktop\api",
+            }
+        }
+
+
+class AnnouncementValidator(BaseModel):
+    title: str = Field(min_length=2)
+    description: str = Field(min_length=2)
+    date: datetime
+    category: AnnouncementsCategory
+    division: str | None = Field(min_length=2)
+
+    @field_validator("date")
+    @classmethod
+    def validate_date_future(cls, v: datetime) -> datetime:
+        if v < datetime.now():
+            raise ValueError("date must be in the future")
+        return v
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "this is an announcement",
+                "description": "this is really an announcement",
+                "date": "2023-04-24T22:01:32.904Z",
+                "category": "internship",
+                "division": "ras",
             }
         }
