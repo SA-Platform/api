bf4ba062a5a432112cfe10cc9d789cb453f28b73
Omar Omran <omaromran2003@gmail.com>
2023-08-14 17:00:19 +0400

    fix oauth2 schema token url fix some type hints

--- api/utils.py
+++ api/utils.py
@@ -1,23 +1,20 @@
 import datetime
-
 from fastapi import HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-
 from api.db.init_db import SessionLocal
 from datetime import timedelta
 from jose import jwt, JWTError
-
 from api.db.models import User
 
 SECRET_KEY = "1e0788a28e2e503315a3a894d353abaa36ace075faae8650f714d7c880f01da5"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="gettoken")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="signin")
 
 
 def get_db():
-    db = SessionLocal()
+    db: Session = SessionLocal()
     try:
         yield db
     finally:
@@ -26,7 +23,7 @@ def get_db():
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     payload = decode_token(token)
-    user = db.query(User).filter_by(username=payload.get("username")).first()
+    user: User | None = db.query(User).filter_by(username=payload.get("username")).first()
     if user:
         return user
     raise HTTPException(
