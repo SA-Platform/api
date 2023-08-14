3496c5637ead424e75b5e918b9caf33a944c458a
Omar Omran <omaromran2003@gmail.com>
2023-08-14 18:21:21 +0400

    add permissions and roles db models

--- api/db/models.py
+++ api/db/models.py
@@ -1,7 +1,9 @@
 import datetime
+from typing import List
+
 import bcrypt
-from sqlalchemy import String, Boolean, DateTime, Integer
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import String, Boolean, DateTime, Integer, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, Relationship
 
 
 class Base(DeclarativeBase):
@@ -74,4 +76,46 @@ class User(Base):
         )"""
 
 
+class Role(Base):
+    __tablename__ = 'roles'
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), unique=True)
+    permissions: Mapped[List["Permission"]] = Relationship("Permission", backref="role")
+
+    def __repr__(self):
+        return f"""Role(
+            "id": {self.id}
+            "name": {self.name}
+        )"""
+
+class Permission(Base):
+    __tablename__ = 'permissions'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    create_task: Mapped[bool]
+    create_announcement: Mapped[bool]
+    create_meeting: Mapped[bool]
+    edit_task: Mapped[bool]
+    edit_announcement: Mapped[bool]
+    edit_meeting: Mapped[bool]
+    respond_to_task: Mapped[bool]
+    set_role: Mapped[bool]
+    edit_role: Mapped[bool]
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"))
+    role: Mapped[User] = Relationship("Role", backref="permissions")
+
+    def __repr__(self):
+        return f"""Permission(
+            "id": {self.id}
+            "create_task": {self.create_task}
+            "create_announcement": {self.create_announcement}
+            "create_meeting": {self.create_meeting}
+            "edit_task": {self.edit_task}
+            "edit_announcement": {self.edit_announcement}
+            "edit_meeting": {self.edit_meeting}
+            "respond_to_task": {self.respond_to_task}
+            "set_role": {self.set_role}
+            "edit_role": {self.edit_role}
+            "role_id": {self.role_id}
+        )"""
