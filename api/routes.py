6478d09e6796087c47e27cd1cff61abeefcf27c8
Omar Omran <omaromran2003@gmail.com>
2023-08-14 16:48:48 +0400

    create get current user dependency

--- api/routes.py
+++ api/routes.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, status, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from api.validators import UserSignup
 from api.db.models import User
-from api.utils import get_db, create_token
+from api.utils import get_db, create_token, get_current_user
 
 app = FastAPI()
 
@@ -13,10 +13,7 @@ async def signup(request: UserSignup, db: Session = Depends(get_db)):
     new_user = User(**request.model_dump())
     db.add(new_user)
     db.commit()
-    return "user created successfully"
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="gettoken")
+    return {"access_token": create_token({"username": new_user.username}), "token_type": "bearer"}
 
 
 @app.post("/signin", tags=["Users"], status_code=status.HTTP_200_OK)
@@ -27,10 +24,10 @@ async def signin(form_data: OAuth2PasswordRequestForm = Depends(), db: Session =
             return {"access_token": create_token({"username": user.username}), "token_type": "bearer"}
         else:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="incorrect password")
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                         detail="user not found")  # for debugging only, should be changed to raise unauthorized always
 
 
-@app.get("/getusers")
-async def get_users(db: Session = Depends(get_db)):
+@app.get("/getusers", tags=["Users"], status_code=status.HTTP_200_OK)
+async def get_users(db: Session = Depends(get_db), user: User = Depends(get_current_user)):
     return db.query(User).all()
--- api/utils.py
+++ api/utils.py
@@ -1,14 +1,19 @@
 import datetime
 
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 from api.db.init_db import SessionLocal
 from datetime import timedelta
 from jose import jwt, JWTError
 
+from api.db.models import User
+
 SECRET_KEY = "1e0788a28e2e503315a3a894d353abaa36ace075faae8650f714d7c880f01da5"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="gettoken")
 
 
 def get_db():
@@ -17,6 +22,18 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_token(token)
+    user = db.query(User).filter_by(username=payload.get("username")).first()
+    if user:
+        return user
+    raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User in token is not found", # debugging only
+            headers={"WWW-Authenticate": "Bearer"},
+    )
 
 
 def create_token(payload: dict, duration: int = ACCESS_TOKEN_EXPIRE_MINUTES):
@@ -28,5 +45,9 @@ def decode_token(token):
     try:
         return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     except JWTError:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
