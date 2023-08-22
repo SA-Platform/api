f95b1c34b604dd1ca0bb32d53b4c10cbf5a1edb1
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-08-23 02:15:46 +0300

    password_reset_email

--- api/utils.py
+++ api/utils.py
@@ -3,6 +3,10 @@ from fastapi import HTTPException, status
 from datetime import timedelta
 from jose import jwt, JWTError
 
+from email.message import EmailMessage
+import ssl 
+import smtplib
+
 SECRET_KEY = "1e0788a28e2e503315a3a894d353abaa36ace075faae8650f714d7c880f01da5"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -22,3 +26,28 @@ def decode_token(token):
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+
+"""Function for sending email to reset the password when the user forgert it"""
+
+"""email_token_password : Not your gmail password, It's a password you generate from your gmail settings"""
+
+def email_sender_for_pass_reset(email_sender, email_token_password, email_receiver):
+    subject = "Reseting Password"
+    
+    # Read the HTML content from the template file
+    with open('./resources/index.html', 'r') as html_file:
+        body = html_file.read()
+
+
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['subject'] = subject
+    em.set_content(body, subtype='html')
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context = context) as smtp:
+        smtp.login(email_sender, email_token_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
