935a93b2b16f5aaf4957fbd1b6e463f1848fac28
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-08-23 12:31:32 +0300

    Imported and used CORSMiddleware to handle cors

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI, status
 from api.routers import divisions, announcements, users, meetings
+from fastapi.middleware.cors import CORSMiddleware
 
 app: FastAPI = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users.usersRouter)
 app.include_router(divisions.divisionsRouter)
 app.include_router(announcements.announcementsRouter)
 app.include_router(meetings.meetingsRouter)
-
 
 if __name__ == "__main__":
     import uvicorn
