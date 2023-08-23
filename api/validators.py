a69e2df141c0d2ff3c792203f2b6d2e0aa2ecb94
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-08-23 21:06:10 +0300

    Added AssignmentValidator class

--- api/validators.py
+++ api/validators.py
@@ -174,3 +174,31 @@ class UsernameValidator(BaseModel):
                 "username": "twibster0x_11"
             }
         }
+
+
+class AssignmentValidator(BaseModel):
+    id: int | None = None
+    title: str = Field(min_length=2)
+    description: str = Field(min_length=2)
+    deadline: datetime
+    attachment: str | None = None
+    weight: int
+    division: str = Field(min_length=2)
+
+    @field_validator("deadline")
+    def validate_date_future(cls, v: datetime) -> datetime:
+        if v < datetime.now(timezone.utc):
+            raise ValueError("date must be in the future")
+        return v
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "this is an assignment",
+                "description": "this is really an assignment",
+                "deadline": "2025-04-24T22:01:32.904Z",
+                "attachment": "assignment.pdf",
+                "weight": "20",
+                "division": "CS",
+            }
+        }
