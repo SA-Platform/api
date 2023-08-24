172a508bde32f38c2b61411fbdf1127fb2ba51ef
Omar Omran <omaromran2003@gmail.com>
2023-08-25 03:56:38 +0400

    reorder routers in main (for docs)

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ app.add_middleware(
 
 app.include_router(users.usersRouter)
 app.include_router(divisions.divisionsRouter)
+app.include_router(assignments.assignmentsRouter)
 app.include_router(announcements.announcementsRouter)
 app.include_router(meetings.meetingsRouter)
-app.include_router(assignments.assignmentsRouter)
+
 
 if __name__ == "__main__":
     import uvicorn
