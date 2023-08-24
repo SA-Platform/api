0741ba642e41d1217b1740ecc216d7c4e03715f3
Omar Omran <omaromran2003@gmail.com>
2023-08-24 19:18:42 +0400

    set field validation for all strings in the validators

--- api/validators.py
+++ api/validators.py
@@ -21,24 +21,24 @@ class HTTPError(BaseModel):
     detail: str
 
     class Config:
-        schema_extra = {
+        json_schema_extra = {
             "example": {"detail": "HTTPException raised."},
         }
 
 
 class UserValidator(BaseModel):
-    first_name: str = Field(min_length=2)
-    last_name: str = Field(min_length=2)
+    first_name: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True, capitalize=True)
+    last_name: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True, capitalize=True)
     birthdate: datetime
-    phone_number: str
+    phone_number: str = Field(min_length=11, max_length=11, pattern=r"^(01)[0-9]{9}$", strip_whitespace=True)
     email: EmailStr
-    username: str = Field(min_length=3)
+    username: str = Field(min_length=3, strip_whitespace=True, pattern=r"^[a-zA-Z0-9_]*$", to_lower=True, strict=True)
     password: str = Field(min_length=8)
     bio: str
-    image_file: str
-    faculty: str
-    university: str
-    faculty_department: str
+    # image_file: str
+    faculty: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True, capitalize=True)
+    university: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True, capitalize=True)
+    faculty_department: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True, capitalize=True)
     graduation_year: int = Field(gt=1900)
 
     class Config:
@@ -56,18 +56,18 @@ class UserValidator(BaseModel):
                 "university": "zagmansoura",
                 "faculty_department": "electrical",
                 "graduation_year": "2025",
-                "image_file": r"C:\Users\omaro\Desktop\api",
+                # "image_file": r"C:\Users\omaro\Desktop\api",
             }
         }
 
 
 class AnnouncementValidator(BaseModel):
     id: int | None = None
-    title: str = Field(min_length=2)
-    description: str = Field(min_length=2)
+    title: str = Field(min_length=2, strip_whitespace=True)
+    description: str = Field(min_length=2, strip_whitespace=True)
     date: datetime | None
     category: AnnouncementsCategory
-    division: str = Field(min_length=2)
+    division: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True)
 
     @field_validator("date")
     def validate_date_future(cls, v: datetime) -> datetime:
@@ -89,13 +89,13 @@ class AnnouncementValidator(BaseModel):
 
 class MeetingValidator(BaseModel):
     id: int | None = None
-    title: str = Field(min_length=2)
+    title: str = Field(min_length=2, strip_whitespace=True)
     description: str = Field(min_length=2)
     date: datetime
     location_text: str | None = None
     location_lat: float | None = None
     location_long: float | None = None
-    division: str = Field(min_length=2)
+    division: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True)
 
     @field_validator("date")
     def validate_date_future(cls, v: datetime) -> datetime:
@@ -127,28 +127,32 @@ class MeetingValidator(BaseModel):
 
 class DivisionValidator(BaseModel):
     id: int | None = None
-    name: str = Field(min_length=2)
-    parent: str | None = None
+    name: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True)
+    parent: str | None = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True)
 
     @model_validator(mode="after")
     def division_exists(self) -> "DivisionValidator":
         db: Session = next(get_db())
         division = db.query(Division).filter_by(name=self.name).first()
         parent = db.query(Division).filter_by(name=self.parent).first()
-        if self.parent:
+        if self.name == self.parent:
+            raise ValueError("division can't be its own parent")
+        elif self.parent:
             if not parent:
                 raise ValueError("parent division doesn't exist")
-
-            if division:
+            elif division:
                 if division.parent == parent:
                     raise ValueError(f"division {division.name} with the same parent ({parent.name}) already exists")
                 elif not division.parent:
                     raise ValueError(f"division {division.name} is a root division and can't have a parent")
             return self
         else:
-            root = db.query(Division).filter_by(parent=None).first()
-            if root:
-                raise ValueError(f"only one root division is allowed, which is {root.name}")
+            if division:
+                raise ValueError(f"division {division.name} already exists")
+            elif not self.id:
+                root = db.query(Division).filter_by(parent=None).first()
+                if root:
+                    raise ValueError(f"only one root division is allowed, which is {root.name}")
             return self
 
     class Config:
@@ -178,11 +182,11 @@ class UsernameValidator(BaseModel):
 
 class AssignmentValidator(BaseModel):
     id: int | None = None
-    title: str = Field(min_length=2)
-    description: str = Field(min_length=2)
+    title: str = Field(min_length=2, strip_whitespace=True)
+    description: str = Field(min_length=2, strip_whitespace=True)
     deadline: datetime
-    attachment: str | None = None
-    weight: int
+    # attachment: str | None = None
+    weight: int = Field(gt=0)
     division: str = Field(min_length=2)
 
     @field_validator("deadline")
@@ -197,7 +201,7 @@ class AssignmentValidator(BaseModel):
                 "title": "this is an assignment",
                 "description": "this is really an assignment",
                 "deadline": "2025-04-24T22:01:32.904Z",
-                "attachment": "assignment.pdf",
+                # "attachment": "assignment.pdf",
                 "weight": "20",
                 "division": "CS",
             }
