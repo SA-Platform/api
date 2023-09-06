77e6dabfa6710fa33710d7c746d683e487859cd8
Omar Omran <omaromran2003@gmail.com>
2023-09-06 22:42:38 +0300

    fix parent must be passed in the body of the request even if null
    Signed-off-by: Omar Omran <omaromran2003@gmail.com>

--- api/validators/division_validator.py
+++ api/validators/division_validator.py
@@ -3,12 +3,12 @@ from pydantic import BaseModel, Field
 
 class DivisionValidator(BaseModel):
     name: str = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True)
-    parent: str | None = Field(min_length=2, strip_whitespace=True, to_lower=True, strict=True)
+    parent: str | None = Field(default=None, min_length=2, strip_whitespace=True, to_lower=True, strict=True)
 
     class Config:
         json_schema_extra = {
             "example": {
                 "name": "CS",
-                "parent": "IEEE",
+                "parent": "IEEE"
             }
         }
