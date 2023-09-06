7a2ae11d23549def1fbd8cc30e7ad7e3220cce4a
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-06 13:38:50 +0300

    Reduced the complicity of "check_division_validity" function

--- api/routes/features_base.py
+++ api/routes/features_base.py
@@ -114,30 +114,41 @@ class Division(CoreBase):
     db_model = DivisionModel
 
     @classmethod
+    def _check_unique_division(cls, division: DivisionModel, parent: DivisionModel) -> None:
+        if division.parent == parent:
+            raise HTTPException(status.HTTP_409_CONFLICT,
+                                detail=f"division {division.name} with the same parent {parent.name} already exists")
+
+    @classmethod
+    def _check_root_division(cls, division: DivisionModel, division_id: int, db: Session) -> None:
+        root = cls.get_db_first(db, "parent", None)
+        if root and root.id != division_id:
+            raise HTTPException(status.HTTP_409_CONFLICT,
+                                detail=f"only one root division is allowed, which is {root.name}")
+
+    @classmethod
     def check_division_validity(cls, request: DivisionValidator, db: Session, division_id: int = None) -> None:
         division = cls.get_db_first(db, "name", request.name)
-        parent = cls.get_db_first(db, "name", request.parent)
+        parent = cls.get_db_first(db, "name", request.parent) if request.parent else None
+
         if request.name == request.parent:
             raise HTTPException(status.HTTP_409_CONFLICT, detail="division can't be its own parent")
-        elif request.parent:
+
+        if request.parent:
             if not parent:
                 raise HTTPException(status.HTTP_404_NOT_FOUND, detail="parent division doesn't exist")
-            elif division:
-                if division.parent == parent:
-                    raise HTTPException(status.HTTP_409_CONFLICT,
-                                        detail=f"division {division.name} with the same parent ({parent.name}) already exists")
-                elif not division.parent:
+
+            if division:
+                cls._check_unique_division(division, parent)
+
+                if not division.parent:
                     raise HTTPException(status.HTTP_409_CONFLICT,
                                         detail=f"division {division.name} is a root division and can't have a parent")
         else:
             if division:
                 raise HTTPException(status.HTTP_409_CONFLICT, detail=f"division {division.name} already exists")
             else:
-                root = db.query(DivisionModel).filter_by(parent=None).first()
-                if root:
-                    if root.id != division_id:
-                        raise HTTPException(status.HTTP_409_CONFLICT,
-                                            detail=f"only one root division is allowed, which is {root.name}")
+                cls._check_root_division(division, division_id, db)
 
 
 class Assignment(FeatureBase):
