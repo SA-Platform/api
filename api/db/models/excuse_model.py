19400b4d93f26bfe4215cf88c7a6727a84144fc0
Omar Omran <omaromran2003@gmail.com>
2023-09-06 22:45:53 +0300

    add update method in excuse db model
    Signed-off-by: Omar Omran <omaromran2003@gmail.com>

--- api/db/models/excuse_model.py
+++ api/db/models/excuse_model.py
@@ -15,11 +15,16 @@ class ExcuseModel(Base):
     description: Mapped[str] = mapped_column(String)
     date_created: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.datetime.now())
     validity: Mapped[datetime] = mapped_column(DateTime(timezone=True))
-    accepted: Mapped[bool] = mapped_column(Boolean)
+    accepted: Mapped[bool] = mapped_column(Boolean, default=False)
 
     # Many-to-one relationships
     creator: Mapped["UserModel"] = Relationship("UserModel", back_populates="excuses")
     assignment: Mapped["AssignmentModel"] = Relationship("AssignmentModel", back_populates="excuses")
+
+    def update(self, description: str, validity: datetime, accepted: bool) -> None:
+        self.description = description
+        self.validity = validity
+        self.accepted = accepted
 
     def __repr__(self):
         return f"""Permission(
