46acadfa180501b94fd25af647ec2ce3b9d65ab9
Omar Omran <omaromran2003@gmail.com>
2023-09-06 22:44:49 +0300

    remove id from role validator
    Signed-off-by: Omar Omran <omaromran2003@gmail.com>

--- api/validators/role_validator.py
+++ api/validators/role_validator.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel, Field, field_validator
 
 
 class RoleValidator(BaseModel):
-    id: int | None = None
     name: str = Field(min_length=2, strip_whitespace=True)
 
     class Config:
