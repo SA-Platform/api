930ca3647e1350baf6561af368362e3e93a36f49
Omar Omran <omaromran2003@gmail.com>
2023-09-08 14:57:54 +0300

    use assignment id for submission and submission id for feedback in the validator
    Signed-off-by: Omar Omran <omaromran2003@gmail.com>

--- api/validators/feedback_validator.py
+++ api/validators/feedback_validator.py
@@ -2,15 +2,16 @@ from pydantic import BaseModel, Field
 
 
 class FeedbackValidator(BaseModel):
-    id: int | None = None
     attachment: str = Field(min_length=2, strip_whitespace=True)
     score: int = Field(gt=0)
     note: str = Field(min_length=2, strip_whitespace=True)
+    submission: int
 
     class Config:
         json_schema_extra = {
             "example": {
-                "attachment": "this is an feedback",
+                "submission": 1,
+                "attachment": "this is a feedback",
                 "score": 20,
                 "note": "this is really a feedback",
             }
--- api/validators/submission_validator.py
+++ api/validators/submission_validator.py
@@ -2,13 +2,14 @@ from pydantic import BaseModel, Field
 
 
 class SubmissionValidator(BaseModel):
-    id: int | None = None
     attachment: str = Field(min_length=2, strip_whitespace=True)
     note: str = Field(min_length=2, strip_whitespace=True)
+    assignment: int
 
     class Config:
         json_schema_extra = {
             "example": {
+                "assignment": 1,
                 "attachment": r"C:\Users\amr\Desktop\api",
                 "note": "this is really an submission",
             }
