3f87c1d8247e942da22844da24fc7fa77a7fd98e
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-08 15:03:04 +0300

    Added Feedbacks and submissions validators

--- api/validators/feedback_validator.py
+++ api/validators/feedback_validator.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, Field
 
 
-class FeedbackValidator(BaseModel):
-    id: int | None = None
+class FeedbackBaseValidator(BaseModel):
+    """This model is used for patch and put requests as it does not include the assignment field"""
     attachment: str = Field(min_length=2, strip_whitespace=True)
     score: int = Field(gt=0)
     note: str = Field(min_length=2, strip_whitespace=True)
@@ -10,8 +10,23 @@ class FeedbackValidator(BaseModel):
     class Config:
         json_schema_extra = {
             "example": {
-                "attachment": "this is an feedback",
+                "attachment": r"C:\Users\amr\Desktop\api",
                 "score": 20,
                 "note": "this is really a feedback",
+            }
+        }
+
+
+class FeedbackValidator(FeedbackBaseValidator):
+    """this model is used for creation as it includes the submission field"""
+    submission: int
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "submission": 1,
+                "attachment": r"C:\Users\amr\Desktop\api",
+                "score": 30,
+                "note": "update this is really a feedback",
             }
         }
--- api/validators/submission_validator.py
+++ api/validators/submission_validator.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, Field
 
 
-class SubmissionValidator(BaseModel):
-    id: int | None = None
+class SubmissionBaseValidator(BaseModel):
+    """This model is used for patch and put requests as it does not include the assignment field"""
     attachment: str = Field(min_length=2, strip_whitespace=True)
     note: str = Field(min_length=2, strip_whitespace=True)
 
@@ -11,5 +11,19 @@ class SubmissionValidator(BaseModel):
             "example": {
                 "attachment": r"C:\Users\amr\Desktop\api",
                 "note": "this is really an submission",
+            }
+        }
+
+
+class SubmissionValidator(SubmissionBaseValidator):
+    """this model is used for creation as it includes the assignment field"""
+    assignment: int
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "assignment": 1,
+                "attachment": r"C:\Users\amr\Desktop\api",
+                "note": "update this submission",
             }
         }
