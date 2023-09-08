ea6dfca4ff9776f58829086ccf095ab0f7487d2b
Omar Omran <omaromran2003@gmail.com>
2023-09-08 22:28:14 +0300

    use permission validator in role validator
    Signed-off-by: Omar Omran <omaromran2003@gmail.com>

--- api/validators/role_validator.py
+++ api/validators/role_validator.py
@@ -1,12 +1,17 @@
 from pydantic import BaseModel, Field
 
+from api.validators.permission_validator import PermissionValidator
+
 
 class RoleValidator(BaseModel):
     name: str = Field(min_length=2, strip_whitespace=True)
+    permissions: PermissionValidator
 
     class Config:
         json_schema_extra = {
             "example": {
                 "name": "chairman",
+                "permissions":
+                    PermissionValidator.Config.json_schema_extra["example"]
             }
         }
