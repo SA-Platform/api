76306444e00529e59d6115f7925bf99c8d53acac
Omar Omran <omaromran2003@gmail.com>
2023-09-08 22:24:49 +0300

    add permission validator
    Signed-off-by: Omar Omran <omaromran2003@gmail.com>

---
+++ api/validators/permission_validator.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel
+
+
+class PermissionValidator(BaseModel):
+    UPDATE_USER: bool
+    DELETE_USER: bool
+    CREATE_DIVISION: bool
+    UPDATE_DIVISION: bool
+    DELETE_DIVISION: bool
+    CREATE_ROLE: bool
+    UPDATE_ROLE: bool
+    DELETE_ROLE: bool
+    CREATE_ASSIGNMENT: bool
+    UPDATE_ASSIGNMENT: bool
+    DELETE_ASSIGNMENT: bool
+    CREATE_ANNOUNCEMENT: bool
+    UPDATE_ANNOUNCEMENT: bool
+    DELETE_ANNOUNCEMENT: bool
+    CREATE_MEETING: bool
+    UPDATE_MEETING: bool
+    DELETE_MEETING: bool
+    CREATE_SUBMISSION: bool
+    UPDATE_SUBMISSION: bool
+    DELETE_SUBMISSION: bool
+    CREATE_EXCUSE: bool
+    UPDATE_EXCUSE: bool
+    DELETE_EXCUSE: bool
+    CREATE_FEEDBACK: bool
+    UPDATE_FEEDBACK: bool
+    DELETE_FEEDBACK: bool
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "UPDATE_USER": True,
+                "DELETE_USER": True,
+                "CREATE_DIVISION": True,
+                "UPDATE_DIVISION": True,
+                "DELETE_DIVISION": True,
+                "CREATE_ROLE": True,
+                "UPDATE_ROLE": True,
+                "DELETE_ROLE": True,
+                "CREATE_ASSIGNMENT": True,
+                "UPDATE_ASSIGNMENT": True,
+                "DELETE_ASSIGNMENT": True,
+                "CREATE_ANNOUNCEMENT": True,
+                "UPDATE_ANNOUNCEMENT": True,
+                "DELETE_ANNOUNCEMENT": True,
+                "CREATE_MEETING": True,
+                "UPDATE_MEETING": True,
+                "DELETE_MEETING": True,
+                "CREATE_SUBMISSION": True,
+                "UPDATE_SUBMISSION": True,
+                "DELETE_SUBMISSION": True,
+                "CREATE_EXCUSE": True,
+                "UPDATE_EXCUSE": True,
+                "DELETE_EXCUSE": True,
+                "CREATE_FEEDBACK": True,
+                "UPDATE_FEEDBACK": True,
+                "DELETE_FEEDBACK": True
+            }
+        }
