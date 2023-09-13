330201b53708e4f560fa4a1ed241eafadd613df1
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-13 22:31:51 +0300

    Fix the case sensitivity in adding division problem

--- api/routes/divisions.py
+++ api/routes/divisions.py
@@ -20,6 +20,8 @@ async def get_divisions(db: Session = Depends(get_db),
 @divisionsRouter.post("/divisions", status_code=status.HTTP_201_CREATED)
 async def create_division(request: DivisionValidator, db: Session = Depends(get_db),
                           _: UserModel = Depends(get_current_user)):
+    request.parent = request.parent.lower() if request.parent else None
+    request.name = request.name.lower()
     Division.check_division_validity(db, request)
     return Division.create(db, name=request.name, parent=db.query(DivisionModel).filter_by(name=request.parent).first())
 
@@ -29,6 +31,8 @@ async def update_division(division_id: int, request: DivisionValidator, db: Sess
                           _: UserModel = Depends(get_current_user)):
     division = db.query(DivisionModel).filter_by(id=division_id).first()  # fetch division to be edited
     if division:
+        request.parent = request.parent.lower() if request.parent else None
+        request.name = request.name.lower()
         Division.check_division_validity(db, request, division_id)
         division.name = request.name  # set its name
         division.parent = db.query(DivisionModel).filter_by(name=request.parent).first()  # set its parent
