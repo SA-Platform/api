f7466580dd772192cb0bc832daf325d4702440f0
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-15 19:34:20 +0300

    Added update assignment validator and used it

--- api/routes/assignments.py
+++ api/routes/assignments.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from api.crud.feature.assignment import Assignment
 from api.db.models import UserModel  # unresolved reference ignored
 from api.dependencies import get_current_user, get_db
+from api.validators import AssignmentValidator, AssignmentUpdateValidator
 
 assignmentsRouter = APIRouter(
     tags=["Assignments"]
@@ -17,13 +18,13 @@ async def get_assignments(db: Session = Depends(get_db),
 
 
 @assignmentsRouter.post("/assignments", status_code=status.HTTP_201_CREATED)
-async def create_assignment(request: Assignment.validator, db: Session = Depends(get_db),
+async def create_assignment(request: AssignmentValidator, db: Session = Depends(get_db),
                             user: UserModel = Depends(get_current_user)):
     return Assignment.create(request, db, user)
 
 
 @assignmentsRouter.put("/assignments/{assignment_id}")
-async def update_assignment(assignment_id: int, request: Assignment.validator, db: Session = Depends(get_db),
+async def update_assignment(assignment_id: int, request: AssignmentUpdateValidator, db: Session = Depends(get_db),
                             _: UserModel = Depends(get_current_user)):
     return Assignment.update(assignment_id, request, db)
 
