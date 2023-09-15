9c13d7bb17dbb33aeefcccbfde7db4faf3430f6e
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-09-15 19:35:34 +0300

    Added update feedback validator and used it

--- api/routes/feedback.py
+++ api/routes/feedback.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from api.crud.sub_feature.feedback import Feedback
 from api.db.models import UserModel, SubmissionModel  # unresolved reference ignored
 from api.dependencies import get_db, get_current_user
-from api.validators import FeedbackValidator, FeedbackBaseValidator
+from api.validators import FeedbackValidator, FeedbackUpdateValidator
 
 feedbacksRouter = APIRouter(
     tags=["Feedback"]
@@ -23,7 +23,7 @@ async def create_feedback(request: FeedbackValidator, db: Session = Depends(get_
 
 
 @feedbacksRouter.put("/feedback/{feedback_id}")
-async def update_feedback(feedback_id: int, request: FeedbackBaseValidator, db: Session = Depends(get_db),
+async def update_feedback(feedback_id: int, request: FeedbackUpdateValidator, db: Session = Depends(get_db),
                           _: UserModel = Depends(get_current_user)):
     return Feedback.update(feedback_id, db,  **request.model_dump())
 
