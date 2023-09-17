94e2558cb96d02ca3c9d7f2309a79caf8631831b
Omar Omran <omaromran2003@gmail.com>
2023-09-17 04:35:38 +0300

    move permission validation to feature base class
    Signed-off-by: Omar Omran <omaromran2003@gmail.com>

--- api/crud/feature/feature_base.py
+++ api/crud/feature/feature_base.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import Session, Mapper
 from starlette import status
 
 from ..core.core_base import CoreBase
-from ...db.models import UserModel, DivisionModel
+from ...const import Permissions
+from ...db.models import UserModel, DivisionModel, UserDivisionPermissionModel
 
 
 class FeatureBase(CoreBase):
@@ -14,6 +15,7 @@ class FeatureBase(CoreBase):
     def create(cls, request: BaseModel, db: Session, user: UserModel) -> Mapper | None:
         request.division = db.query(DivisionModel).filter_by(name=request.division).first()
         if request.division:
+            cls._compare_permissions(user, cls._fetch_permission_value("CREATE"), request.division, db)
             return super().create(db, creator=user, **request.model_dump())
         raise HTTPException(status.HTTP_404_NOT_FOUND, "division not found")
 
@@ -24,8 +26,38 @@ class FeatureBase(CoreBase):
         if model:
             request.division = db.query(DivisionModel).filter_by(name=request.division).first()
             if request.division:
+                cls._compare_permissions(user, cls._fetch_permission_value("UPDATE"), request.division, db)
                 cls.db_update(model, **request.model_dump())
                 db.commit()
-                return {"msg": "updates saved"}
+                db.refresh(model)
+                return model
             raise HTTPException(status.HTTP_404_NOT_FOUND, detail="division not found")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{cls.__name__.lower()} not found")
+
+    @classmethod
+    def delete(cls, model_id: int, db: Session, user: UserModel) -> dict:
+        model = cls.get_db_first(db, "id", model_id)
+        if model:
+            cls._compare_permissions(user, cls._fetch_permission_value("DELETE"), model.division, db)
+            db.delete(model)
+            db.commit()
+            return {"msg": f"{cls.__name__.lower()} deleted"}
+
+    @classmethod
+    def _fetch_permission_value(cls, action: str) -> int:
+        return getattr(Permissions, f"{action}_{cls.__name__.upper()}")
+
+    @classmethod
+    def _compare_permissions(cls, user: UserModel,
+                             permission_to_check: int,
+                             division: DivisionModel,
+                             db: Session) -> UserModel:
+
+        user_permissions: UserDivisionPermissionModel | None = db.query(UserDivisionPermissionModel).filter_by(
+            user=user,
+            division=division).first()
+        if user_permissions:
+            if (user_permissions.permissions & permission_to_check) == permission_to_check:
+                return user
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"this user does not have the permission to do this action in {division.name} division")
